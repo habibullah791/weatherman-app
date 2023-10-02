a274d2095348d6612ead09b4f0702a665844d477
habibullah791 <whoishabib791@gmail.com>
2023-10-02 13:30:54 +0500

    ADD - Comment to __init__.py file and UPDATE .gitignore

---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+# This is the __init__.py file for the 'my_package' package.
+# 
+# Python treats directories containing an __init__.py file as packages.
+# This file can be used to define package-level attributes, perform
+# package initialization, and organize related modules within the package.
+#
+# It allows you to create a modular and organized namespace for your code.
+# Import statements in this file can make package modules accessible
+# when you import 'my_package' in other parts of your Python project.
